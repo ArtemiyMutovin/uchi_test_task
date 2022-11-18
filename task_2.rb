ee3390a7989e72ff4b7c7aadf9745a79cbd5493d
Artemiy Mutovin <artemmutovin@mail.ru>
2022-11-18 11:56:09 -0500

    обновление 2 задания

--- task_2.rb
+++ task_2.rb
@@ -3,35 +3,22 @@
 # ======(a)======
 
 def keys_array(arr)
-  new_array = []
-
-  arr.each do |hash|
-    new_array << hash.keys
-  end
-  new_array.flatten
+  arr.map {|a| a.keys}.flatten
 end
 
 # ======(b)======
 
 def values_array(arr)
-  new_array = []
-
-  arr.each do |hash|
-    new_array << hash.values
-  end
-  new_array.flatten
+  arr.map {|a| a.values}.flatten
 end
 
 # ======(c)======
 
 def values_sum_array(arr)
-  new_array = []
-
-  arr.each do |hash|
-    new_array << hash.values
-  end
-  new_array.flatten.sum
+  arr.map {|a| a.values}.flatten.sum
 end
+
+
 
 arr = [{a: 1, b: 2, c: 45}, {d: 123, c: 12}, {e: 87}]
 
